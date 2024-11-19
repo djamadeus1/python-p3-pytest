120d8b14f763f129d4df491d71be5720bc69c96d
Ronald Roberts <djamadeus1@gmail.com>
2024-11-19 14:09:20 -0500

    whatever

--- lib/testing/test_not_none.py
+++ lib/testing/test_not_none.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
-from not_none_functions import return_not_none
+from bool_functions import return_true
+
+def test_return_true():
+    '''in bool_functions, function "return_true" returns True.'''
+    assert return_true() == True
 
 # def test_return_not_none():
 #     '''in not_none_functions, function "return_not_none" returns a value that is not None.'''
